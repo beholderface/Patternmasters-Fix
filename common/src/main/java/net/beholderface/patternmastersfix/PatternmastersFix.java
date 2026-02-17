2d24a0ad660334409f1b0e5d96383c8614ac2221
beholderface <13290805+beholderface@users.noreply.github.com>
2026-02-17 10:03:29 -0500

    make it work

--- common/src/main/java/net/beholderface/patternmastersfix/PatternmastersFix.java
+++ common/src/main/java/net/beholderface/patternmastersfix/PatternmastersFix.java
@@ -1,9 +1,13 @@
 package net.beholderface.patternmastersfix;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public final class PatternmastersFix {
-    public static final String MOD_ID = "patternmasters-fix";
+    public static final String MOD_ID = "patternmasters_fix";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     public static void init() {
-        // Write common init code here.
+        LOGGER.info("fixing patternmaster's");
     }
 }
--- common/src/main/java/net/beholderface/patternmastersfix/mixin/PatternmasterFixMixin.java
+++ common/src/main/java/net/beholderface/patternmastersfix/mixin/PatternmasterFixMixin.java
@@ -1,4 +1,20 @@
 package net.beholderface.patternmastersfix.mixin;
 
+import at.petrak.hexcasting.api.casting.PatternShapeMatch;
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
+import at.petrak.hexcasting.api.casting.math.HexPattern;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import ram.talia.moreiotas.common.casting.actions.strings.OpActionString;
+
+@Mixin(OpActionString.class)
 public class PatternmasterFixMixin {
+    @WrapOperation(method = "execute", at = @At(value = "INVOKE",
+            target = "Lat/petrak/hexcasting/common/casting/PatternRegistryManifest;matchPattern(Lat/petrak/hexcasting/api/casting/math/HexPattern;Lat/petrak/hexcasting/api/casting/eval/CastingEnvironment;Z)Lat/petrak/hexcasting/api/casting/PatternShapeMatch;",
+    remap = false), remap = false)
+    public PatternShapeMatch matchPatternNoAlternateOrders(HexPattern key, CastingEnvironment env, boolean environment, Operation<PatternShapeMatch> original){
+        return original.call(key, env, false);
+    }
 }
